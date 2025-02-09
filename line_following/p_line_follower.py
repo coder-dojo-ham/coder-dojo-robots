8ce8868568b359d8fc865e4fba426fc4a076b255
Danny Staple <danny@orionrobots.co.uk>
2025-02-09 13:01:54 +0000

    Line follower used in demo - needs fixing.

---
+++ line_following/p_line_follower.py
@@ -0,0 +1,44 @@
+from machine import ADC, Pin
+import time
+import motors
+
+line_in = ADC(Pin(27, Pin.IN))
+
+SENSOR_MID = 2 ** 15
+# Normalise to -1.0 to 1.0
+SENSOR_SCALE = 1 / SENSOR_MID
+SPEED = 0.3
+# How much to correct motors by
+PROPORTION = 0.2
+
+class Proportional:
+    def __init__(self, proportion):
+        self.kp = proportion
+
+    def calculate(self, error):
+        return self.kp * error
+
+
+def read_sensor():
+    raw = line_in.read_u16()
+    mid = raw - SENSOR_MID
+    scaled = mid * SENSOR_SCALE
+    return scaled
+
+
+def run():
+    corrector = Proportional(PROPORTION)
+    try:
+        while True:
+            # We can directly call it an error, if it's not 0, it's not in the middle.
+            error = read_sensor()
+            correction = corrector.calculate(error)
+            print(error, correction, SPEED + correction, SPEED - correction)
+            motors.set_speed(motors.left, SPEED + correction)
+            motors.set_speed(motors.right, SPEED - correction)
+            time.sleep(0.1)
+    finally:
+        motors.stop_all()
+        
+if __name__ == "__main__":
+    run()
---
+++ line_following/pid_line_follower.py
@@ -0,0 +1,43 @@
+from machine import ADC, Pin
+import time
+import motors
+
+line_in = ADC(Pin(27, Pin.IN))
+btn_20 = Pin(20, mode=Pin.IN, pull=Pin.PULL_UP)
+
+class PID:
+    def __init__(self, kp=0.1, ki=0, kd=0, d_filter_gain=0.1):
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+        self.d_filter_gain = d_filter_gain
+        self.integral = 0
+        self.error_prev = 0
+
+    def calculate(self, error, dt):
+        self.integral += error * dt
+        difference = (error - self.error_prev) * self.d_filter_gain
+        return self.kp * error + self.ki * self.integral + self.kd * difference
+
+
+print("Waiting to go...")
+# while btn_20.value() == 1:
+# time.sleep(0.1)
+
+speed = 0.3
+line_pid = PID(0.2)
+current_time = time.ticks_ms()
+scale_factor = 1 / 32768
+try:
+    while True:
+        last_time = current_time
+        line_error = (line_in.read_u16() - 32768) * scale_factor
+        current_time = time.ticks_ms()
+        dt = time.ticks_diff(current_time, last_time)
+        output = line_pid.calculate(line_error, dt)
+        print(line_error, output, speed + output, speed - output)
+        motors.set_speed(motors.left, speed + output)
+        motors.set_speed(motors.right, speed - output)
+        time.sleep(0.1)
+finally:
+    motors.stop_all()
