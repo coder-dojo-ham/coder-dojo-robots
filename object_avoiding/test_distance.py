41039fd0904ad42fe432a3ddbb0a4f6204597c0c
Danny Staple <danny@orionrobots.co.uk>
2025-02-09 13:00:56 +0000

    Adapt to current design

--- object_avoiding/test_distance.py
+++ object_avoiding/test_distance.py
@@ -1,14 +1,13 @@
 # test distance sensors
-from machine import Pin
 import time
 
 from hcsr04 import HCSR04
 
-left_distance_sensor = HCSR04(4, 5)
-right_distance_sensor = HCSR04(6, 7)
+right_sensor = HCSR04(trigger_pin=0, echo_pin=1)
+left_sensor = HCSR04(trigger_pin=7, echo_pin=28)
 
 while True:
-    left_distance = left_distance_sensor.distance_mm()
-    right_distance = right_distance_sensor.distance_mm()
+    left_distance = right_sensor.distance_cm()
+    right_distance = left_sensor.distance_cm()
     print(left_distance, right_distance)
     time.sleep(0.1)
