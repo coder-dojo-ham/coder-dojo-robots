648b0e50b278f55105aace4f32efba0fbea01f27
Danny Staple <danny@orionrobots.co.uk>
2025-02-09 13:01:21 +0000

    Cute and reusable larson scanner for maker pi rp2040

--- playing_with_leds/larson_scanner.py
+++ playing_with_leds/larson_scanner.py
@@ -1,21 +1,23 @@
 from machine import Pin
 import time
 
+
 def larson_leds():
     current_direction = 1
-    current_position = 0
+    current_position = 1
     position_led_range = list(range(0, 8)) + [16, 17, 26, 27, 28]
-    position_leds = [Pin(l, Pin.OUT) for l in position_led_range]
+    position_leds = [Pin(led, Pin.OUT) for led in position_led_range]
     highest_position = len(position_leds) - 1
     while True:
         if not 0 < current_position < highest_position:
             current_direction = -current_direction
         position_leds[current_position].value(0)
-        current_position += current_direction        
+        current_position += current_direction
         position_leds[current_position].value(1)
         yield
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     larson = larson_leds()
     while True:
         next(larson)
