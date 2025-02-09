df9c5f84b3cd2768c0da51f62c361205a3e6a451
Danny Staple <danny@orionrobots.co.uk>
2025-02-09 13:29:56 +0000

    Line follower fixed for demonstration mode

--- line_following/p_line_follower.py
+++ line_following/p_line_follower.py
@@ -2,7 +2,6 @@ from machine import ADC, Pin
 import time
 import motors
 
-line_in = ADC(Pin(27, Pin.IN))
 
 SENSOR_MID = 2 ** 15
 # Normalise to -1.0 to 1.0
@@ -18,8 +17,7 @@ class Proportional:
     def calculate(self, error):
         return self.kp * error
 
-
-def read_sensor():
+def read_sensor(line_in):
     raw = line_in.read_u16()
     mid = raw - SENSOR_MID
     scaled = mid * SENSOR_SCALE
@@ -27,11 +25,12 @@ def read_sensor():
 
 
 def run():
+    line_in = ADC(Pin(27, Pin.IN))
     corrector = Proportional(PROPORTION)
     try:
         while True:
             # We can directly call it an error, if it's not 0, it's not in the middle.
-            error = read_sensor()
+            error = read_sensor(line_in)
             correction = corrector.calculate(error)
             print(error, correction, SPEED + correction, SPEED - correction)
             motors.set_speed(motors.left, SPEED + correction)
@@ -39,6 +38,6 @@ def run():
             time.sleep(0.1)
     finally:
         motors.stop_all()
-        
+
 if __name__ == "__main__":
     run()
