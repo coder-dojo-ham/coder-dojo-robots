0d2974625755a19a52ac83e1386b79b6f8b8353c
dannystaple <danny@orionrobots.co.uk>
2025-03-11 18:05:45 +0000

    Quick tool to install demonstration code on the robot

---
+++ install_demo.bat
@@ -0,0 +1,6 @@
+mpremote cp demonstration_mode.py :main.py
+mpremote cp motors.py :motors.py
+mpremote cp playing_with_leds\larson_scanner.py :larson_scanner.py
+mpremote cp object_avoiding\wall_avoid_proportional.py :wall_avoid_proportional.py
+mpremote cp line_following\p_line_follower.py :p_line_follower.py
+mpremote cp 3rd_party\hcsr04.py :hcsr04.py
